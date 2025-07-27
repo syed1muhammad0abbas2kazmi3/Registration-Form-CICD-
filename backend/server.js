3dda0dc4a7bef29ee0e9e8fb9f17365478d7b0bb
syed1muhammad0abbas2kazmi3 <syedk3549@gmail.com>
2025-07-27 16:38:06 +0000

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,40 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient } = require("mongodb");
 
 const PORT = 5050;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
 const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
 const client = new MongoClient(MONGO_URL);
 
-//GET all users
-app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Also useful if using JSON from frontend
+app.use(express.static(path.join(__dirname, "../frontend")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+// GET all users
+app.get("/getUsers", async (req, res) => {
+    await client.connect(MONGO_URL);
     const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
+    const data = await db.collection("users").find({}).toArray();
     client.close();
     res.send(data);
 });
 
-//POST new user
+// POST new user
 app.post("/addUser", async (req, res) => {
     const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
-
+    await client.connect(MONGO_URL);
     const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
+    const result = await db.collection("users").insertOne(userObj);
     client.close();
+    res.send({ message: "User added successfully", data: result });
 });
-
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
+
